63657748272e1374f9455396c407a2c5e2bd1b10
chase-jefferson <jefferson.chase18@gmail.com>
2025-03-23 13:44:17 -0400

    Updated vite.config.ts to prevent Vite from blocking requests

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,7 @@ export default defineConfig({
     host: '0.0.0.0',  
     port: 3000,        
   },
+  preview: {
+    allowedHosts: ['candidate-search-07y7.onrender.com', 'localhost'],  
+  },
 });
